4130498447557ffdf4bb8eadeb192bdf58238851
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2021-04-24 21:32:36 +0600

    simple navigation with react done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, { useState } from "react";
 import "./styles.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import { Link } from "react-router-dom";
 
 export default function App() {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const menus = ["Home", "About", "Contact"];
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <header>
+      <div className="container">
+        <nav>
+          <div className="logo">The Navigation</div>
+          <ul className={`${menuToggle && "slide"}`}>
+            {menus.map((menu) => (
+              <li>
+                <Link to="#" onClick={() => setMenuToggle(false)}>
+                  {menu}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="mobile" onClick={() => setMenuToggle(!menuToggle)}>
+            {!menuToggle ? <MenuIcon /> : <CloseIcon />}
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 }
